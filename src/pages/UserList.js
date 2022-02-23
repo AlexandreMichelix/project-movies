582bd277e1f90a4a37c029f14a57f0fca57a8494
Alexandre Michelix <alexandre.michelix@dotic.fr>
2022-02-23 20:54:27 +0100

    Add films to favorites pages

--- src/pages/UserList.js
+++ src/pages/UserList.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import Header from '../components/Header';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "../components/Card";
+import Header from "../components/Header";
 
 const UserList = () => {
-    return (
-        <div>
-            <Header />
-        </div>
-    );
+  const [listData, setListData] = useState([]);
+
+  useEffect(() => {
+    let moviesId = window.localStorage.movies
+      ? window.localStorage.movies.split(",")
+      : [];
+
+    for (let i = 0; i < moviesId.length; i++) {
+      axios
+        .get(
+          `https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=edc914e05ded85fa9c4da95db5d306de&language=en-US&external_source=imdb_id`
+        )
+        .then((res) => setListData((listData) => [...listData, res.data]));
+    }
+  }, []);
+
+  return (
+    <div className="user-list-page">
+      <Header />
+      <h2>
+        Favoris <span>🙏</span>
+      </h2>
+      <div className="result">
+        {listData.length > 0 ? listData.map((movie) => (
+          <Card movie={movie} key={movie.id} />
+        )) : <h2>Aucun favoris pour le moment</h2> }
+      </div>
+    </div>
+  );
 };
 
 export default UserList;
